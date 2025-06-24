c69289ce08e5a2c09baa1769a50dad4b91172468
lassetotl <113028933+lassetotl@users.noreply.github.com>
2025-06-24 11:02:37 +0200

    Add files via upload

--- src/anova analyse.py
+++ src/anova analyse.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns; sns.set()
 
+from statsmodels.formula.api import ols
+from statsmodels.stats.anova import anova_lm
+from matplotlib.lines import Line2D
 ''' 
 data = {'subject_id': [1, 1, 1, 2, 2, 2, 3, 3, 3],
         'time_point': [1, 2, 3, 1, 2, 3, 1, 2, 3],
@@ -30,14 +33,16 @@ df['ID'] = ID
 #df = df.set_index('ID')
 #%%
 
-from statsmodels.formula.api import ols
-from statsmodels.stats.anova import anova_lm
-
-param = 'c_std'
+param = 'GCSRs'
 formula = f'{param} ~ Day + ID'
 
 df_sham = df[df['Condition']==0]
 model_sham = ols(formula, data=df_sham).fit()
+#print(model_sham.summary())
+slope_sham = model_sham.params.iloc[-1] # indexing to exclude intercept and Day
+std_sham = model_sham.bse.iloc[-1]
+print(f'OLS regression, sham: ({slope_sham} \pm {std_sham})')
+
 anova_table_sham = anova_lm(model_sham)
 print(f'ANOVA results {param} (sham): \n', anova_table_sham, '\n')
 P_sham = anova_table_sham['PR(>F)']['Day']  # P-verdi for endring over dager
@@ -46,18 +51,27 @@ P_sham = anova_table_sham['PR(>F)']['Day']  # P-verdi for endring over dager
 df_mi = df[df['Condition']==1]
 model_mi = ols(formula, data=df_mi).fit()
 anova_table_mi = anova_lm(model_mi)
+#print(model_mi.summary())
+slope_mi = model_mi.params.iloc[-1] # indexing to exclude intercept and Day
+std_mi = model_mi.bse.iloc[-1]
+print(f'OLS regression, mi: ({slope_mi} \pm {std_mi})')
+
 print(f'ANOVA results {param} (mi): \n', anova_table_mi, '\n')
 P_mi = anova_table_mi['PR(>F)']['Day']  # P-verdi for endring over dager
 
 print(f'Endring over tid for {param}: \n Sham: {np.round(P_sham, 3)} \n MI: {np.round(P_mi, 3)}')
 
 
-plt.figure(figsize=(7, 6), dpi=300)
-plt.title(f'repeated measures ANOVA, linear model \n Sham: p = {np.round(P_sham, 3)} \n MI: p = {np.round(P_mi, 3)}')
+f = plt.figure(figsize=(6, 5), dpi=200)
+#plt.title('Repeated measures ANOVA, OLS linear model')
 
 # paletter, html-koder
 mi_palette = ['#852F30', '#9B3637', '#B03D3E', '#C1494A', '#C95D5E', '#D07273', '#D88788', '#DF9C9C', '#E6B1B1']
 sham_palette = ['#373C9B', '#3E44B1', '#4B51C1', '#5F64C9', '#7478D0', '#898CD8', '#9DA1DF', '#B3B5E6', '#C8CAEE']
+
+# labels
+legend_handles1 = [Line2D([0], [0], color = sham_palette[1], lw = 2, label = fr'$\beta_1$ = {np.round(slope_sham, 3)}, p = {np.round(P_sham, 3)}', marker = 'o'),
+          Line2D([0], [0], color = mi_palette[1], lw = 2, label = fr'$\beta_1$ = {np.round(slope_mi,3)}, p = {np.round(P_mi, 3)}', marker = 'v')]
 
 # plotte linjer over tid for hvert individ
 individer = set(df['ID'])
@@ -78,13 +92,9 @@ for id_ in individer:
         sham_i += 1
         marker = 'o'
     plt.plot(days_sorted, param_sorted, c=color, marker=marker)
-    plt.xlabel('Days'); plt.ylabel(param)
-    #plt.plot(df_sham[])
-    
-plt.show()
 
-#%%
-#POST HOC analyse
-# identifisere for hvilke rotter målingene endres signifikant
-#tukey_oneway_sham = pairwise_tukeyhsd(endog = df_sham[param], groups = df_sham['Day'])
-#tukey_oneway_mi = pairwise_tukeyhsd(endog = df_mi[param], groups = df_mi['Day'])
+plt.xlabel('Days')#; plt.ylabel(param)
+plt.ylabel(r'GC-SRs [$s^{-1}$]')
+#plt.ylabel(r'$\theta_{compression, diastole} \ [^{\circ}]$', fontsize = 15)
+plt.legend(handles=legend_handles1, prop={'size': 12})
+plt.show()
